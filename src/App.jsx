5260d29fede36f24dd1a4dedce6dea29f54b784d
RhettRoseman <rroseman@rcplanners.com>
2023-11-09 21:24:16 -0500

    compnents folder and contents of about.jsx and aboutForm.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      head<h1>About me</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
